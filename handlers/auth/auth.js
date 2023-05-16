9baf295c464fbe49f41c3d75c91a6f67c09f1fa5
Marvin <josehenriquez.niu@gmail.com>
2023-05-16 10:36:18 -0600

    useless code removed

--- handlers/auth/auth.js
+++ handlers/auth/auth.js
@@ -4,23 +4,6 @@ const bcrypt = require('bcryptjs');
 
 require("dotenv").config()
 
-exports.handler = async(event) => {
-    const data = event.queryStringParameters && event.queryStringParameters.access_token
-
-    const accessToken = data.split("access_token=")[1].split('&')[0]
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Headers": (process.env.HEADERS).toString(),
-            "Access-Control-Allow-Origin": (process.env.ORIGIN).toString(),
-            "Access-Control-Allow-Methods": (process.env.METHODS).toString()
-        },
-        body: JSON.stringify(accessToken)
-    }
-
-}
-
 module.exports.registerUser = async(event) => {
     const data = JSON.parse(event.body);
     const prisma = new PrismaClient();
